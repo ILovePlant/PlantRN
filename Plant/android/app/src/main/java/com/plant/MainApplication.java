27a42b18dfb801ec24749894fbf01ab7c5656ed8
zqm <zhangqm@ucacc.com>
2016-12-05 11:36:58 +0800

    添加“react-native-svg”

--- Plant/android/app/src/main/java/com/plant/MainApplication.java
+++ Plant/android/app/src/main/java/com/plant/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.horcrux.svg.RNSvgPackage;
 
 import cn.reactnative.modules.update.UpdateContext;
 
@@ -33,7 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new UpdatePackage()
+            new UpdatePackage(),
+            new RNSvgPackage()
       );
     }
   };
--- Plant/index.ios.js
+++ Plant/index.ios.js
@@ -51,5 +51,51 @@ import {
 // });
 
 import Plant from './scr/Plant';
+import Svg,{
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    // Text,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg';
 
-AppRegistry.registerComponent('Plant', () => Plant);
+class SvgExample extends Component {
+    render() {
+        return (
+            <Svg
+                height="100"
+                width="100"
+            >
+                <Circle
+                    cx="50"
+                    cy="50"
+                    r="45"
+                    stroke="blue"
+                    strokeWidth="2.5"
+                    fill="green"
+                />
+                <Rect
+                    x="15"
+                    y="15"
+                    width="70"
+                    height="70"
+                    stroke="red"
+                    strokeWidth="2"
+                    fill="yellow"
+                />
+            </Svg>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Plant', () => SvgExample);
